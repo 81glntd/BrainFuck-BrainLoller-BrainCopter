2fd46cae7b1a00ce0e7fe7a38db1cf36823a6256
syky27 <syky27@gmail.com>
2013-03-06 13:25:18 +0100

    fuction for reading brainfuck code from file

--- bf_interpreter/main.py
+++ bf_interpreter/main.py
@@ -1,9 +1,11 @@
 __author__ = 'Tomas Sykora sykorto6@fit.cvut.cz'
+import sys
 
-
-def file_source 
-
+def file_source(filename):
+    with open(sys.argv, encoding='utf-8') as bf_source_file:
+        source = bf_source_file.read()
 
 
 if __name__ == "__main__":
-
+    if sys.argv:
+        file_source(sys.argv)
