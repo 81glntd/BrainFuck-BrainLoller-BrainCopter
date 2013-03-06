d3ea4e912e535e88008b1b27fb63d8795b4112c8
syky27 <syky27@gmail.com>
2013-03-06 14:02:47 +0100

    Yeah!!! Finally working piece of code, reading BrainFuck source code from file and also from prompt

--- bf_interpreter/main.py
+++ bf_interpreter/main.py
@@ -1,16 +1,23 @@
 __author__ = 'Tomas Sykora sykorto6@fit.cvut.cz'
 import sys
 
+
 def file_source(filename):
+    """
+    Returns variable source which contains brainfuck source code
+    file_source is handling brainfuck source code from file using 'switcher' -f
+    :param filename:
+    """
     with open(filename, encoding='utf-8') as bf_source_file:
         source = bf_source_file.read()
         print(source)
 
 
 if __name__ == "__main__":
-    if sys.argv[1] == '-f':
-        file_source(sys.argv[2])
-    elif sys.argv[1] == None:
-        print("neni soubor")
+    NONARGV = []
+    if sys.argv[1:] == NONARGV:
+        print("You have not chosen any file please enter BrainFuck code manualy")
         source = input()
         print(source)
+    else: #sys.argv[1] == '-f':
+        file_source(sys.argv[2])
