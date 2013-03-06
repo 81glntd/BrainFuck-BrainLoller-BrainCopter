7349bf03f7cf8f3d51ad47d55beddf3f138e3bef
syky27 <syky27@gmail.com>
2013-03-06 13:30:44 +0100

    fixing opening file to argument 1

--- bf_interpreter/main.py
+++ bf_interpreter/main.py
@@ -4,8 +4,9 @@ import sys
 def file_source(filename):
     with open(sys.argv, encoding='utf-8') as bf_source_file:
         source = bf_source_file.read()
+        print(source)
 
 
 if __name__ == "__main__":
     if sys.argv:
-        file_source(sys.argv)
+        file_source(sys.argv[1])
