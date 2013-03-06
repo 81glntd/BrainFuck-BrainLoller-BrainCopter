b6ee9f02e299ffedf4ad2fdbcadf92040b3fe7e2
syky27 <syky27@gmail.com>
2013-03-06 13:39:25 +0100

    fixing opening file to argument -f file

--- bf_interpreter/main.py
+++ bf_interpreter/main.py
@@ -2,11 +2,15 @@ __author__ = 'Tomas Sykora sykorto6@fit.cvut.cz'
 import sys
 
 def file_source(filename):
-    with open(sys.argv, encoding='utf-8') as bf_source_file:
+    with open(filename, encoding='utf-8') as bf_source_file:
         source = bf_source_file.read()
         print(source)
 
 
 if __name__ == "__main__":
-    if sys.argv:
-        file_source(sys.argv[1])
+    if sys.argv[1] == '-f':
+        file_source(sys.argv[2])
+    elif sys.argv[1] == None:
+        print("neni soubor")
+        source = input()
+        print(source)
