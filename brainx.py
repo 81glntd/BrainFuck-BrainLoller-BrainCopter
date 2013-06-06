5519e2eb934ebf8ef8cfbcc3f23ec4c8fb48140d
syky27 <syky27@gmail.com>
2013-06-06 16:47:29 +0200

    using my old interpreter

--- brainx.py
+++ brainx.py
@@ -12,12 +12,18 @@ class BrainFuck:
         self.data = data
         
         # initialization of varialbles
-        self.memory = memory
+        self.memory = bytearray(memory)
         self.memory_pointer = memory_pointer
         
         # DEBUG and tests
         # a) output memory
         self.output = ''
+
+        try:
+            with open(data, mode='r') as f:
+                self.code = f.read()
+        except:
+            self.code = data
     
     #
     # for test purposes
@@ -25,6 +31,49 @@ class BrainFuck:
     def get_memory(self):
         # Do not forget to change this acording to your implementation
         return self.memory
+
+    def _interpret(self, code):
+        """This is where the magic is going on"""
+        i = 0
+        while p < len(code):
+            if source[source_position] == '+':
+            output.increase()
+        elif source[source_position] == '-':
+            output.decrease()
+        elif source[source_position] == '>':
+            output.move_right()
+        elif source[source_position] == '<':
+            output.move_left()
+        elif source[source_position] == ',':
+            output.set(ord(sys.stdin.read(1)))
+        elif source[source_position] == '[':
+            if output.get() == 0:
+                loop_counter = 1
+                while loop_counter > 0:
+                    source_position += 1
+                    helper = source[source_position]
+                    if helper == '[':
+                        loop_counter += 1
+                    elif helper == ']':
+                        loop_counter -= 1
+        elif source[source_position] == ']':
+            loop_counter = 1
+            while loop_counter > 0:
+                source_position -= 1
+                helper = source[source_position]
+                if helper == '[':
+                    loop_counter -= 1
+                elif helper == ']':
+                    loop_counter += 1
+            source_position -= 1
+        elif source[source_position] == '.':
+            print(chr(output.get()), end=r'')
+        elif source[source_position] == ',':
+            if 0 <= source_position < 30000:
+                x = input()
+                output.set(x)
+                source_position += 1
+        source_position += 1
 
 
 class BrainLoller():
