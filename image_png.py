9114d35a088802ad58cbd77e70f56fc26faf21b3
syky27 <syky27@gmail.com>
2013-06-13 19:13:08 +0200

    Starting to work on PNG

--- image_png.py
+++ image_png.py
@@ -16,6 +16,15 @@ class PngReader():
     """Třída pro práci s PNG-obrázky."""
     
     def __init__(self, filepath):
+        with open(filepath, mode='rb') as f:
+            self.png_source = f.read()
+
+        #check if the file is PNG
+        if self.png_source[:8] != b'\x89\x50\x4e\x47\x0d\x0a\x1a\x0a':
+            raise PNGWrongHeaderError()
+
+
+
         
         # RGB-data obrázku jako seznam seznamů řádek,
         #   v každé řádce co pixel, to trojce (R, G, B)
