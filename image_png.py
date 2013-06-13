84943dd2592f7c87ca3c73b975bac408ae1b2998
syky27 <syky27@gmail.com>
2013-06-13 19:35:04 +0200

    Chunk is done

--- image_png.py
+++ image_png.py
@@ -23,7 +23,15 @@ class PngReader():
         if self.png_source[:8] != b'\x89\x50\x4e\x47\x0d\x0a\x1a\x0a':
             raise PNGWrongHeaderError()
 
-
+    def _read_png(self):
+        pointer = 8
+        self.chunks = []
+        while pointer < len(self.png_source):
+            h = self.png_source[pointer:pointer+4]
+            chunk_size = h[0] * 256 ** 3 + h[1] * 256 ** 2 + h[2] * 256 + h[3]
+            self.chunks += [{'type': self.png_source[pointer + 4:pointer + 8],
+                             'data': self.png_source[pointer + 8: pointer + 8 + chunk_size]}]
+            pointer += 12 + chunk_size
 
         
         # RGB-data obrázku jako seznam seznamů řádek,
