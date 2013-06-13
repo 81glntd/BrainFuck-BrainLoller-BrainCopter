312cd3cf209d9c10ca2f777047fd394d0a75dc9d
syky27 <syky27@gmail.com>
2013-06-13 22:22:30 +0200

    I can now play Lost kingdom game

--- brainx.py
+++ brainx.py
@@ -45,16 +45,19 @@ class BrainFuck:
 
             #incrementation
             if c[c_i] == '+':
-                self.memory[self.memory_pointer] += 1
+                if self.memory[self.memory_pointer] != 255:
+                    self.memory[self.memory_pointer] += 1
                 #too much
-                if self.memory[self.memory_pointer] == 256:
+                else:
                     self.memory[self.memory_pointer] = 0
 
             #decrementation
             elif c[c_i] == '-':
-                self.memory[self.memory_pointer] -= 1
-                if self.memory[self.memory_pointer] == -1:
+                if self.memory[self.memory_pointer] != 0:
+                    self.memory[self.memory_pointer] -= 1
+                else:
                     self.memory[self.memory_pointer] = 255
+
 
             #right shift
             elif c[c_i] == '>':
@@ -241,20 +244,23 @@ class BrainCopter():
         self.program = BrainFuck(self.data)
 
 if __name__ == "__main__":
-    from optparse import OptionParser
-    usage = "usage: %prog [options] arg1 arg2"
-    parser = OptionParser(usage=usage)
-    parser.add_option("-v", "--verbose",
-                      action="store_true", dest="verbose", default=True,
-                      help="make lots of noise [default]")
-    parser.add_option("-q", "--quiet",
-                      action="store_false", dest="verbose",
-                      help="be vewwy quiet (I'm hunting wabbits)")
-    parser.add_option("-f", "--filename",
-                      metavar="FILE", help="write output to FILE")
-    parser.add_option("-m", "--mode",
-                      default="intermediate",
-                      help="interaction mode: novice, intermediate, "
-                           "or expert [default: %default]")
+    BrainCopter('test_data/lk.png')
+
+
+    # from optparse import OptionParser
+    # usage = "usage: %prog [options] arg1 arg2"
+    # parser = OptionParser(usage=usage)
+    # parser.add_option("-v", "--verbose",
+    #                   action="store_true", dest="verbose", default=True,
+    #                   help="make lots of noise [default]")
+    # parser.add_option("-q", "--quiet",
+    #                   action="store_false", dest="verbose",
+    #                   help="be vewwy quiet (I'm hunting wabbits)")
+    # parser.add_option("-f", "--filename",
+    #                   metavar="FILE", help="write output to FILE")
+    # parser.add_option("-m", "--mode",
+    #                   default="intermediate",
+    #                   help="interaction mode: novice, intermediate, "
+    #                        "or expert [default: %default]")
 
 
